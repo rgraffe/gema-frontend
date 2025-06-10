05fa6b5fb218435f673aea205b681e8076fd17c3
jesusarl <jarlyc28@gmail.com>
2025-06-10 17:43:48 -0400

    Grupos de Trabajo y otros

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,40 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import App from "./App";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import App from "./App"
+import Canva from "./pages/Canva"
+import VistaGeneral from "./pages/VistaGeneral"
+import UbicacionesTecnicas from "./pages/UbicacionesTecnicas"
+import GruposTrabajo from "./components/GruposTrabajo"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Canva />,
+      },
+      {
+        path: "general",
+        element: <VistaGeneral />,
+      },
+      {
+        path: "locations",
+        element: <UbicacionesTecnicas />,
+      },
+      {
+        path: "groups",
+        element: <GruposTrabajo />,
+      },
+    ],
   },
-]);
+])
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
-);
+)
